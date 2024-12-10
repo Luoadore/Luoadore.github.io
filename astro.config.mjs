7a737b95edf8874839daaaaa0763b6b8aa13a331
luoadore <luoluomayday@gmail.com>
2024-12-10 18:38:18 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site: 'https://luoadore.github.io',
+	site: 'https://Luoadore.github.io',
 });
